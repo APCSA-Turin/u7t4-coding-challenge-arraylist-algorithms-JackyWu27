bf22b34036009e3d1c01e22fd185202c50086ef7
JackyWu27 <jackyw27@nycstudents.net>
2025-02-06 13:49:47 +0000

    frist

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -15,7 +15,12 @@ public class Main{
     *  @param str String to insert
     */
     public static ArrayList<String> insertAfterI(ArrayList<String> stringList, String str){
-       return new ArrayList<String>();
+        for (int c = 0; c < stringList.size(); c++){
+            if (stringList(c).indexOf("i") != -1) {
+                stringList.add(c, str);
+            }
+        }
+        return stringList;
     }
 
 
@@ -28,6 +33,12 @@ public class Main{
    *  @param stringList  original arraylist of Strings
    */
     public static ArrayList<String> removeThree(ArrayList<String> stringList){
+        for (int c = 0; c < stringList.size(); c ++){
+            if (s.length() == 3) {
+                stringList.remove(c);
+                c--;
+            }
+        }
         return stringList;
     }
 
@@ -41,7 +52,11 @@ public class Main{
    *  @param intList  original array of integers
    */
     public static ArrayList<Integer> reverseArray(int[] intList){
-        return new ArrayList<Integer>();
+        ArrayList<Integer> i = new ArrayList<Integer>();
+        for (int c : intList) {
+            i.add(0, c);
+        }
+        return c;
     }
 
 
@@ -59,6 +74,10 @@ public class Main{
      *  @param wordList  arraylist of Strings
      */
     public static ArrayList<String> duplicateUpperEnd(ArrayList<String> wordList){
+        int limit = wordList.size();
+        for (int c = 0; c < limit; c ++) {
+            wordlist.add(wordList(c).toUppercase());
+        }
         return wordList;
     }
 
@@ -78,7 +97,14 @@ public class Main{
    */
 
     public static ArrayList<String> parseSentence(String sentence){
-        return new ArrayList<String>();
+        ArrayList<String> a = new ArrayList<String>();
+        int c = sentence.indexOf(" ");
+        while (c != -1) {
+            a.add(s.substring(0, c));
+            String s = sentence.substring(c + 1);
+            c = sentence.indexOf(" ");
+        }
+        return a;
     }
 
 
@@ -99,6 +125,15 @@ public class Main{
    *  @param wordList  arraylist of words
    */
     public static ArrayList<String> moveBWords(ArrayList<String> wordList){
+        int count = 0;
+        for (int c = 0; c < wordList.size(); c ++) {
+            if (wordList(c).substring(0,1) == "b" || wordList(c).substring(0,1) == "B") {
+             String s = wordList(c);
+             wordList.remove(c);
+             wordList.add(count, s);
+             count ++;
+            }
+        }
         return wordList;
     }
 
@@ -114,6 +149,13 @@ public class Main{
      *  @param intList  intList of Integers
      */
     public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> intList){
+        ArrayList<Integer> i = new ArrayList<Integer>();
+        for (int c = 0; c < intList.size(); c ++) {
+            if (!i.contains(intList(c))) {
+                i.add(intList(c));
+            }
+        }
+        intList = i;
         return intList;
     }
 
@@ -125,6 +167,12 @@ public class Main{
     // sameFirstLast([1, 2, 1]) → true
     //sameFirstLast([]) -> false
     public static boolean sameFirstLast(ArrayList<Integer> list){
+        if (list.size() > 0) {
+          return false;  
+        }
+        if (list(0) == list(list.size() - 1)) {
+            return true;
+        }
         return false;
     }
 
@@ -137,6 +185,9 @@ public class Main{
     // swapEnds([8, 6, 7, 9, 5]) → [5, 6, 7, 9, 8]
     // swapEnds([]->[])
     public static ArrayList<Integer> swapEnds(ArrayList<Integer> list){        
+        int c = list(0);
+        list.set(0, list.size() - 1);
+        list.set(list.size(), c);
         return list;
     }
 
@@ -150,6 +201,12 @@ public class Main{
     // zeroFront([0, 1, 1, 0, 1]) → [0, 0, 1, 1, 1]
     // zeroFront([1, 0]) → [0, 1]
     public static ArrayList<Integer> zeroFront(ArrayList<Integer> list){
+        for (int c = 0; c < list.size(); c ++) {
+            if (list(c) == 0) {
+             wordList.remove(c);
+             wordList.add(0, 0);
+            }
+        }
         return list;
     }
 
@@ -161,8 +218,21 @@ public class Main{
     // of the given value which is alone is replaced by whichever value to its left or right is larger.
     // notAlone([1, 2, 3], 2) → [1, 3, 3]
     // notAlone([1, 2, 3, 2, 5, 2], 2) → [1, 3, 3, 5, 5, 5]
-    // notAlone([3, 4], 3) → [3, 3]
+    // notAlone([3, 4], 3) → [4, 4]
     public static ArrayList<Integer> notAlone(ArrayList<Integer> list, int val){
+        for (int c = 0; c < list.size(); c++) {
+            if (list(c) == val) {
+                if (c == 0) {
+                    list.set(c, list(c + 1));
+                } else if (c == list.size() -1) {
+                    list.set(c, list(c - 1));
+                } else if (list(c + 1) > list(c - 1)) {
+                    list.set(c, list(c + 1));
+                } else {
+                    list.set(c, list(c - 1));
+                }
+            }
+        }
         return list;
     }
 
@@ -175,6 +245,8 @@ public class Main{
     // shiftLeft([1]) → [1]
 
     public static ArrayList<Integer> shiftLeft(ArrayList<Integer> list){
+        int c = list.remove(0);
+        list.add(c);
         return list;
     }
     
@@ -189,6 +261,26 @@ public class Main{
     // fix34([3, 2, 2, 4]) → [3, 4, 2, 2]
 
     public static ArrayList<Integer> fix34(ArrayList<Integer> list){
+        boolean check1 = false;
+        boolean check2 = false;
+        int l1 = -1;
+        int l2 = -1;
+        for (int c = 0; c < list.size(); c++) {
+            if (list(c) == 3) {
+                l1 = c;
+                check1 = true;
+            }
+            if (list(c) == 4) {
+                l2 = c;
+                check2 = true;
+            }
+            if (check1 && check2) {
+                check1 = false;
+                check2 = false;
+                list.remove(l2);
+                list.add(l1 + 1, 4);
+            }
+        }
         return list;
     }
 
